42ea4d1ddfcc7aaf21287d4b9336162331871880
Paul Rossotti <rossotti.paul@gmail.com>
2015-09-21 06:04:40 -0700

    introduce additional hello methods

--- chirp-java/src/main/java/com/rossotti/chirp/resources/HelloResource.java
+++ chirp-java/src/main/java/com/rossotti/chirp/resources/HelloResource.java
@@ -1,19 +1,42 @@
 package com.rossotti.chirp.resources;
 
+import javax.ws.rs.BadRequestException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-//http://localhost:8080/chirp-web/rest/publish/hello%20world
-@Path("/publish")
+@Path("/hello")
 public class HelloResource {
 
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response sayHello(@QueryParam("name") String name, @QueryParam("age") String ageString) {
+		try {
+			int age = (ageString == null) ? 0 : Integer.valueOf(ageString);
+			String msg = (name == null) ? "Hello!" : "Hello " + name + " welcome to " + age;
+			return Response.ok(msg).build();
+
+		} catch (NumberFormatException e) {
+			throw new BadRequestException("I need an int silly", e);
+		}
+	}
+	
     @GET
-    @Path("/{message}")
-    public Response publishMessage(@PathParam("message") String msgStr){
-         
-        String responseStr = "Received message: "+msgStr;
+    @Path("/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response sayHelloWithName(@PathParam("name") String name){         
+        String responseStr = "Hello "+ name + "!";
         return Response.status(200).entity(responseStr).build();
     }
+    
+	@GET
+	@Path("/{name}/{age}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayHelloWithName(@PathParam("name") String name, @PathParam("age") int age) {
+		return "Hello " + name + ", welcome to " + age;
+	}
 }
