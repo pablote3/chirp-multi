a770d601500018425915fcd1382fc04cf686ea1c
Paul Rossotti <rossotti.paul@gmail.com>
2015-09-21 06:02:38 -0700

    register HelloResource as class

--- chirp-java/src/main/java/com/rossotti/chirp/app/ChirpApplication.java
+++ chirp-java/src/main/java/com/rossotti/chirp/app/ChirpApplication.java
@@ -1,6 +1,8 @@
 package com.rossotti.chirp.app;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.ws.rs.ApplicationPath;
@@ -11,14 +13,34 @@ import com.rossotti.chirp.resources.HelloResource;
 @ApplicationPath("/")
 public class ChirpApplication extends Application {
 
-    private Set<Object> singletons = new HashSet<Object>();
+	protected final Set<Class<?>> classes = new HashSet<>();
+    private Set<Object> singletons = new HashSet<>();
+    protected final Map<String, Object> properties = new HashMap<>();
 
     public ChirpApplication() {
-        singletons.add(new HelloResource());
+    	registerClasses();
+    }
+    
+    private void registerClasses() {
+    	classes.add(HelloResource.class);
+    }
+    
+    @Override
+    public Set<Class<?>> getClasses() {
+      return classes;
     }
 
     @Override
     public Set<Object> getSingletons() {
         return singletons;
+    }
+    
+    @Override
+    public Map<String, Object> getProperties() {
+      return properties;
+    }
+    
+    public void setProperty(String name, Object value) {
+        properties.put(name, value);
     }
 }
